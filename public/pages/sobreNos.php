600e951f88a5dc0248b5e3f3726005057e128dfe
Domingos <domingoscapewa08@gmail.com>
2025-05-18 01:21:02 -0300

    feat: adicionar estilos personalizados para a página "Sobre Nós" e vincular o novo arquivo CSS

--- public/pages/sobreNos.php
+++ public/pages/sobreNos.php
@@ -3,40 +3,14 @@
 require_once '../../includes/config.php';
 require_once '../../includes/header.php';
 ?>
-<!-- <link rel ="stylesheet" href="/assets/styles/style.css" /> -->
-<!-- RAYAN
-    <div class="about" id="about">
-      <div class="left">
-        <img src="../../assets/img/sobre_empresa.jpg" />
-      </div>
-      <div class="right">
-        <h3>Nossa Visão para um futuro melhor</h3>
-        <p>
-          A G-TECH é uma empresa brasileira especializada em sistemas e soluções
-          de desenvolvimento web. Fundada com a missão de oferecer soluções
-          tecnológicas de ponta para empresas e empreendedores, estamos
-          comprometidos com a excelência e a inovação em tudo o que fazemos.
-          Nossa equipe é formada por profissionais altamente qualificados e
-          apaixonados por tecnologia, prontos para enfrentar os desafios mais
-          complexos e entregar resultados excepcionais.
-        </p>
-        <p>
-          Experiência Comprovada: Com anos de experiência no mercado, possuímos
-          um histórico sólido de sucesso e satisfação do cliente. Equipe
-          Talentosa: Nossa equipe é composta por profissionais altamente
-          qualificados e dedicados, prontos para enfrentar qualquer desafio.
-        </p>
-      </div>
-    </div>
-  </body> -->
+<link rel="stylesheet" href="../../assets/styles/sobreNos.css" />
 
 <!DOCTYPE>
 <html>
 
 <head>
   <title>Sobre Nós</title>
-  <link rel="../../style.css"
-    </head>
+</head>
 
 <body>
   <section class="sobre-nos">
@@ -83,7 +57,7 @@ require_once '../../includes/header.php';
 
 </body>
 
-<style>
+<!-- <style>
   /* Vídeo */
   .video-section {
     background-color: #eef3f6;
@@ -202,6 +176,6 @@ require_once '../../includes/header.php';
     color: #005599;
     margin-bottom: 10px;
   }
-</style>
+</style> -->
 
 </html>
